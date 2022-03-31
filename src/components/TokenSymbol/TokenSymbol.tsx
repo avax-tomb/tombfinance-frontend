73d208d72e1e901359f153f9f98403a061f421f8
alonavns <alona.vns@outlook.com>
2022-03-31 14:01:06 +0300

    warning

--- src/components/TokenSymbol/TokenSymbol.tsx
+++ src/components/TokenSymbol/TokenSymbol.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import styled, { ThemeContext } from 'styled-components';
+import styled from 'styled-components';
 
 //Graveyard ecosystem logos
 import tombLogo from '../../assets/img/tokens/TOMB.png';
--- src/hooks/useBank.ts
+++ src/hooks/useBank.ts
@@ -4,7 +4,7 @@ import { Bank } from '../tomb-finance';
 
 const useBank = (sectionInUI: Number, poolId: Number): Bank => {
   const { banks } = useContext(BanksContext);
-  return banks.find((bank) => bank.sectionInUI == sectionInUI && bank.poolId === poolId);
+  return banks.find((bank) => bank.sectionInUI === sectionInUI && bank.poolId === poolId);
 };
 
 export default useBank;
